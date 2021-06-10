dc2dfd76a5ff7149196d1d708572040cabd29da6
rexren <905578725@qq.com>
2021-06-10 09:56:17 +0800

    所有按钮使用多线程方式实现

--- main.py
+++ main.py
@@ -55,25 +55,31 @@ class Ui_MainWindow(QMainWindow):
         self.del_device_buttom = QPushButton("Del_Device", self)
         self.del_device_buttom.setGeometry(460, 180, 121, 41)
         self.del_device_buttom.clicked.connect(self.del_device)
+        #List router
+        self.list_router_label = QLabel(self)
+        self.list_router_label.setText(
+            'List Temperature Data:''<a href=http://127.0.0.1:8123/listener/eventlog>''&nbsp; <b>http://127.0.0.1:8123/listener/eventlog</b>''</a>')
+        self.list_router_label.setOpenExternalLinks(True)
+        self.list_router_label.setGeometry(10, 215, 600, 50)
         #list temp
         self.list_temp_label=QLabel(self)
         self.list_temp_label.setText('List Temperature Data:''<a href=http://127.0.0.1:8123/listener/temperature>''&nbsp; <b>http://127.0.0.1:8123/listener/temperature</b>''</a>')
         self.list_temp_label.setOpenExternalLinks(True)
-        self.list_temp_label.setGeometry(10,215,600,50)
+        self.list_temp_label.setGeometry(10,240,600,50)
         #list ECG
         self.list_ecg_label=QLabel(self)
         self.list_ecg_label.setText('List ECG Data:''<a href=http://127.0.0.1:8123/listener/ecg>''&nbsp; <b>http://127.0.0.1:8123/listener/ecg</b>''</a>')
         self.list_ecg_label.setOpenExternalLinks(True)
-        self.list_ecg_label.setGeometry(10,240,600,50)
+        self.list_ecg_label.setGeometry(10,265,600,50)
         # list SpO2
         self.list_spo2_label = QLabel(self)
         self.list_spo2_label.setText(
             'List SpO2 Data:''<a href=http://127.0.0.1:8123/listener/spo2>''&nbsp; <b>http://127.0.0.1:8123/listener/spo2</b>''</a>')
         self.list_spo2_label.setOpenExternalLinks(True)
-        self.list_spo2_label.setGeometry(10, 265, 600, 50)
+        self.list_spo2_label.setGeometry(10, 290, 600, 50)
         # 日志栏
         self.log_textEdit=QTextEdit(self)
-        self.log_textEdit.setGeometry(30,310,500,460)
+        self.log_textEdit.setGeometry(30,340,500,430)
         self.show()
 
     def show_testEdit(self,str):
@@ -94,55 +100,29 @@ class Ui_MainWindow(QMainWindow):
 
     def add_device(self):
         self.log_textEdit.clear()
-        try:
-            requestbody=[eval(self.add_device_textEdit.toPlainText())]
-            url = "http://localhost:8123/devices"
-            payload = json.dumps(requestbody)
-            headers = {
-                'Content-Type': 'application/json'
-            }
-            response = requests.request("POST", url, headers=headers, data=payload)
-            if response.json()['message'] == 'success':
-                self.log_textEdit.append(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}添加成功')
-            else:
-                self.log_textEdit.append(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}添加失败')
-        except Exception as e:
-            self.log_textEdit.append(e)
+        self.thread = Add_device(self.add_device_textEdit)
+        self.thread.trigger.connect(self.show_testEdit)
+        self.thread.start()
+
     def del_device(self):
         self.log_textEdit.clear()
-        try:
-            requestbody=[{"deviceId":eval(self.add_device_textEdit.toPlainText())['deviceId']}]
-            url = "http://localhost:8123/devices"
-            payload = json.dumps(requestbody)
-            headers = {
-                'Content-Type': 'application/json'
-            }
-            response = requests.request("DELETE", url, headers=headers, data=payload)
-            if response.json()['message'] == 'success':
-                self.log_textEdit.append(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}删除成功')
-            else:
-                self.log_textEdit.append(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}删除失败')
-        except Exception as e:
-            self.log_textEdit.append(e)
+        self.thread = Del_device(self.add_device_textEdit)
+        self.thread.trigger.connect(self.show_testEdit)
+        self.thread.start()
+
     def show_router(self):
         self.log_textEdit.clear()
-        url = "http://localhost:8123/routers"
-        payload = {}
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        for router in response.json()['data']:
-            self.log_textEdit.append(str(router).replace(",",'\n').replace('{','').replace("}",'').replace(" ",''))
-            self.log_textEdit.append("----------------------------------------")
+        self.thread = Show_router()
+        self.thread.trigger.connect(self.show_testEdit)
+        self.thread.start()
+
 
     def show_device(self):
         self.log_textEdit.clear()
-        url = "http://localhost:8123/devices"
-        payload = ""
-        headers = {}
-        response = requests.request("GET", url, headers=headers, data=payload)
-        for device in response.json()['data']:
-            self.log_textEdit.append(str(device).replace(",", '\n').replace('{', '').replace("}", '').replace(" ", ''))
-            self.log_textEdit.append("----------------------------------------")
+        self.thread = Show_device()
+        self.thread.trigger.connect(self.show_testEdit)
+        self.thread.start()
+
 
 class Add_router(QThread):
     trigger = pyqtSignal(str)
@@ -189,6 +169,79 @@ class Del_router(QThread):
                 self.trigger.emit(f'{eval(self.add_router_textEdit.toPlainText())["deviceId"]}删除失败')
         except Exception as e:
             self.trigger.emit(str(e))
+
+class Add_device(QThread):
+    trigger = pyqtSignal(str)
+    def __init__(self,add_device_textEdit):
+        super(Add_device,self).__init__()
+        self.add_device_textEdit=add_device_textEdit
+
+    def run(self):
+        try:
+            requestbody=[eval(self.add_device_textEdit.toPlainText())]
+            url = "http://localhost:8123/devices"
+            payload = json.dumps(requestbody)
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            if response.json()['message'] == 'success':
+                self.trigger.emit(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}添加成功')
+            else:
+                self.trigger.emit(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}添加失败')
+        except Exception as e:
+            self.trigger.emit(str(e))
+
+class Del_device(QThread):
+    trigger = pyqtSignal(str)
+    def __init__(self,add_device_textEdit):
+        super(Del_device, self).__init__()
+        self.add_device_textEdit=add_device_textEdit
+
+    def run(self):
+        try:
+            requestbody=[{"deviceId":eval(self.add_device_textEdit.toPlainText())['deviceId']}]
+            url = "http://localhost:8123/devices"
+            payload = json.dumps(requestbody)
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            response = requests.request("DELETE", url, headers=headers, data=payload)
+            if response.json()['message'] == 'success':
+                self.trigger.emit(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}删除成功')
+            else:
+                self.trigger.emit(f'{eval(self.add_device_textEdit.toPlainText())["deviceName"]}删除失败')
+        except Exception as e:
+            self.trigger.emit(str(e))
+
+class Show_router(QThread):
+    trigger = pyqtSignal(str)
+    def __init__(self):
+        super(Show_router, self).__init__()
+
+    def run(self):
+        url = "http://localhost:8123/routers"
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        for router in response.json()['data']:
+            self.trigger.emit(str(router).replace(",", '\n').replace('{', '').replace("}", '').replace(" ", ''))
+            self.trigger.emit("----------------------------------------")
+
+class Show_device(QThread):
+    trigger = pyqtSignal(str)
+    def __init__(self):
+        super(Show_device, self).__init__()
+
+    def run(self):
+        url = "http://localhost:8123/devices"
+        payload = ""
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        for device in response.json()['data']:
+            self.trigger.emit(str(device).replace(",", '\n').replace('{', '').replace("}", '').replace(" ", ''))
+            self.trigger.emit("----------------------------------------")
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Ui_MainWindow()
